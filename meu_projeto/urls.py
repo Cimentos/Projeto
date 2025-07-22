c64f488dde47229da4c55e2fe2c532760467b12c
rphillip11 <pedrophp697@gmail.com>
2025-07-22 15:49:03 -0300

    de novo5

--- meu_projeto/urls.py
+++ meu_projeto/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from sistema import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'), 
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('sistema.urls')),
 ]
